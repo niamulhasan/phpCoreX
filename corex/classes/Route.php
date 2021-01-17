dd4e493f2182c612d113b8ee8918cf7a12f27504
niamulhasan <niamulhasanbd@gmail.com>
2021-01-18 04:50:12 +0600

    Get method added in Route.php class and Function.php class added

--- corex/classes/Route.php
+++ corex/classes/Route.php
@@ -21,4 +21,47 @@ class Route
             $function->__invoke();
         }
     }
+
+    public static function get($route, $function)
+    {
+        self::$routeList[] = $route;
+
+        $path = $_SERVER['REQUEST_URI'];
+        $path = substr($path, strlen(Config::get('app_variables/root_url')) + 2);
+        //echo "url::" . $path . "<br>";
+        //echo "route::" . $route . "<br>";
+        //echo "url array::";
+        //print_r(explode("/", $path));
+        //echo "<br/>";
+        //echo "route array::";
+        //print_r(explode("/", $route));
+        //echo "<br/><br/>";
+        $path_array = explode("/", $path);
+        $route_array = explode("/", $route);
+
+        $ifFullMatch = false;
+        $params = array();
+
+        if (count($path_array) == count($route_array)) {
+            foreach ($route_array as $index => $route_part) {
+                if (!(Functions::startsWith($route_part, "$"))) {
+                    if ($route_part == $path_array[$index]) {
+                        $ifFullMatch = true;
+                    }
+                } else {
+                    $params[$route_part] = $path_array[$index];
+                }
+            }
+        }
+
+        //print_r($params);
+        //echo "<br/>";
+
+        if ($ifFullMatch) {
+            $function->__invoke($params);
+        }
+        // if ($path == $route) {
+        //     $function->__invoke(array("one", "two"));
+        // }
+    }
 }
